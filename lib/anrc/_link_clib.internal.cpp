83124a26bfdf4da956370922fade54752a66c81f
Anar Bastanov <69395109+anar-bastanov@users.noreply.github.com>
2025-06-11 21:38:26 +0400

    Add more functions and constants to local C library

--- lib/anrc/_link_clib.internal.cpp
+++ lib/anrc/_link_clib.internal.cpp
@@ -2,26 +2,22 @@
 
 extern "C"
 {
-    #include <cstdio>
     #include <cmath>
+    #include <cfenv>
+    #include <cstdio>
     #include <ctime>
-    #include <cwchar>
     #include <clocale>
 }
 
 extern "C"
 {
-    int __c_LC_ALL      = LC_ALL;
-    int __c_LC_COLLATE  = LC_COLLATE;
-    int __c_LC_CTYPE    = LC_CTYPE;
-    int __c_LC_MONETARY = LC_MONETARY;
-    int __c_LC_NUMERIC  = LC_NUMERIC;
-    int __c_LC_TIME     = LC_TIME;
+    #define __c_const(const_name) auto __cc_##const_name = const_name;
+    #include "_clib_consts.internal.txt"
 }
 
 constexpr const std::initializer_list<void *> &_ =
 {
     nullptr
-    #define extern ,(void*)
-    #include "_clib.internal.inc"
+    #define __c_func ,(void*)
+    #include "_clib_funcs.internal.txt"
 };
